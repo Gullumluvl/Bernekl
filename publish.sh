0c8cccc2d07e85eb61b4236e6a11cf1956cc75cd
GullumLuvl <7593801+Gullumluvl@users.noreply.github.com>
2018-04-30 19:26:27 +0200

    better option parsing (now options can be put before or after the argument). Check that it did do something before quitting.

--- publish.sh
+++ publish.sh
@@ -2,12 +2,29 @@
 
 set -euo pipefail
 
-destfile="$1" || echo "Missing arguments: <destfile> [<pandoc options> ...]" >&2
-shift
+### Read command line arguments
+options=()
+destfile="-"
+
+while [[ "$destfile" =~ ^-.* ]]; do
+    [ "$destfile" != "-" ] && options+=("$destfile")
+    
+    destfile="$1" || {
+        echo "Missing arguments: <destfile> [<pandoc options> ...]" >&2;
+        exit 1;
+    }
+    shift
+done
+
+options+=($@)
+
+### Infer input file and convert
 
 basefilename="${destfile%.*}"
 # pandoc version > 2.0:
 # extension "smart" and "fenced_code_attributes" enabled by default.
+
+done=0
 
 for ext in "mkd" "md" "markdown" "txt"; do
     if [ -f "$basefilename.$ext" ]; then
@@ -16,8 +33,11 @@ for ext in "mkd" "md" "markdown" "txt"; do
             -c css/buttondown.css \
             --lua-filter=task-list.lua \
             --standalone \
-            $@ \
+            ${options[@]} \
             -o "$destfile" "$basefilename.$ext"
+        done=1
         break
     fi
 done
+
+(( "$done" )) || { echo "Error: no source file was found!"; exit 1;}
